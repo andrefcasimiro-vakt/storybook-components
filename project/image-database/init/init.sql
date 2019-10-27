f3042dbc800e35dd0c6608be7f20420604b1516c
André Fernandes <andre.fernandes@VAKT.io>
2019-10-27 17:34:28 +0000

    - Added react web service to docker

--- project/image-database/init/init.sql
+++
@@ -1,9 +0,0 @@
-GRANT ALL PRIVILEGES ON DATABASE midikrew TO midikrew;
-
-\c midikrew;
-
-DROP TABLE IF EXISTS users;
-CREATE TABLE users(id serial PRIMARY KEY, name VARCHAR(255));
-
-
-INSERT INTO users(name) VALUES('user');
